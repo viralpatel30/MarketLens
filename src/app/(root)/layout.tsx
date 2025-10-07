a1dc0da28548a88020a895239103f7caa1bb705c
Viral Patel <pviral3011@gmail.com>
2025-10-07 23:39:35 +0530

    feat: restructure application layout and add header component
    - Removed the old Home component and created a new layout for the application.
    - Introduced a Header component that includes navigation items and a user dropdown.
    - Added NavItems component to manage navigation links dynamically.
    - Implemented UserDropdown for user-related actions with avatar support.
    - Integrated Radix UI components for dropdown and avatar functionalities.
    - Defined navigation items in a constants file for better management.
    - Updated package.json to include new dependencies for Radix UI components.

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "@/components/Header";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="min-h-screen text-gray-400">
+      <Header />
+      <div className="conatainer py-10">{children}</div>
+    </main>
+  );
+};
+
+export default layout;
